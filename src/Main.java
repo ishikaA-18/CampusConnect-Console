2f634bbfb823b1abecdd23acfa87417a2a780c77
Ishika Agarwal <itsishikaagarwal18@gmail.com>
2026-05-13 11:28:10 +0530

    Added password protection for admin mode

--- src/Main.java
+++ src/Main.java
@@ -86,49 +86,58 @@ public class Main {
                 break;
                 case 2:
                     System.out.println("Admin Mode Activated!!");
-                    System.out.println("Welcome Admin!\nTo Proceed further enter your choice-\nPress 1 to add a new teacher.\nPress 2 to delete a teacher.\nPress 3 to update teacher logic\nPress 4 to exit admin mode.");
-                    int admin_input=sc.nextInt();
-                    sc.nextLine();
-                    switch(admin_input){
-                        case 1:
-                            System.out.println("Adding a new teacher...");
-                            System.out.println("Enter the name of the new teacher:");
-                            String new_teacher_name = sc.nextLine();
-                            System.out.println("Enter the department of the new teacher in capital letters:");
-                            String new_teacher_department=sc.nextLine();
-                            System.out.println("Enter the block and floor number where the new teacher will be sitting in:");
-                            String new_teacher_room = sc.nextLine();
-                            System.out.println("Enter the designation of the new teacher:");
-                            String new_teacher_designation = sc.nextLine();
-                            System.out.println("Enter the subject name the new teacher will be primarily teaching:");
-                            String new_teacher_subject = sc.nextLine();
-                            facultyList.add(new Faculty(new_teacher_name,new_teacher_department,new_teacher_room,new_teacher_designation,new_teacher_subject));
-                            try{
-                                BufferedWriter new_entry=new BufferedWriter(new FileWriter("data.csv", true));
-                                String s=new_teacher_name+","+new_teacher_department+","+new_teacher_room+","+new_teacher_designation+","+new_teacher_subject;
-                                new_entry.write(s);
-                                new_entry.newLine();
-                                new_entry.close();
-                            }    
-                            catch(Exception e){
-                                System.out.println("File not found.");
-                            }
-                            System.out.println("New Teacher Added Succesfully!");
-                        break;
-                        case 2:
+                    String password="AOT169";
+                    System.out.println("Enter the password");
+                    String password_input=sc.nextLine();
+                    if(password_input.equals(password)){
+                        System.out.println("Welcome Admin!\nTo Proceed further enter your choice-\nPress 1 to add a new teacher.\nPress 2 to delete a teacher.\nPress 3 to update teacher logic\nPress 4 to exit admin mode.");
+                        int admin_input=sc.nextInt();
+                        sc.nextLine();
+                        switch(admin_input){
+                            case 1:
+                                System.out.println("Adding a new teacher...");
+                                System.out.println("Enter the name of the new teacher:");
+                                String new_teacher_name = sc.nextLine();
+                                System.out.println("Enter the department of the new teacher in capital letters:");
+                                String new_teacher_department=sc.nextLine();
+                                System.out.println("Enter the block and floor number where the new teacher will be sitting in:");
+                                String new_teacher_room = sc.nextLine();
+                                System.out.println("Enter the designation of the new teacher:");
+                                String new_teacher_designation = sc.nextLine();
+                                System.out.println("Enter the subject name the new teacher will be primarily teaching:");
+                                String new_teacher_subject = sc.nextLine();
+                                facultyList.add(new Faculty(new_teacher_name,new_teacher_department,new_teacher_room,new_teacher_designation,new_teacher_subject));
+                                try{
+                                    BufferedWriter new_entry=new BufferedWriter(new FileWriter("data.csv", true));
+                                    String s=new_teacher_name+","+new_teacher_department+","+new_teacher_room+","+new_teacher_designation+","+new_teacher_subject;
+                                    new_entry.write(s);
+                                    new_entry.newLine();
+                                    new_entry.close();
+                                }    
+                                catch(Exception e){
+                                    System.out.println("File not found.");
+                                }
+                                System.out.println("New Teacher Added Succesfully!");
+                            break;
+                            case 2:
 
-                        break;
-                        case 3:
+                            break;
+                            case 3:
 
+                            break;
+                            case 4:
+                                System.out.println("Admin Mode Deactivated!!");
+                            break;
+                            default:
+                                System.out.println("Wrong Choice");
+                        }
+                    }
+                    else{
+                        System.out.println("Wrong Password.........Returning back to main menu");
                         break;
-                        case 4:
-                            System.out.println("Admin Mode Deactivated!!");
-                        break;
-                        default:
-                            System.out.println("Wrong Choice");
                     }
                 break;
-             }
+            }
         }
     }
 }
